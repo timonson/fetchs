224afd9bf1bdee322626c6f8245250b06bb04193
timonson <timo.rossa@tutanota.com>
2021-04-02 02:09:05 +0200

    Update

--- tests/example.ts
+++
@@ -1,7 +0,0 @@
-import { fetchS, fetchSPolyfill } from "../mod.ts";
-
-const r1 = await fetchS("https://github.com", { bodyMethod: "text" });
-console.log(r1);
-
-const r2 = await fetchSPolyfill(import.meta.url, { bodyMethod: "blob" });
-console.log(r2);
--- tests/fetchs_polyfilled_test.ts
+++ tests/fetchs_polyfilled_test.ts
@@ -1,5 +1,12 @@
 import { assertEquals } from "https://deno.land/std@0.91.0/testing/asserts.ts";
-import { fetchSPolyfill } from "../mod.ts";
+import { fetchS, fetchSPolyfill } from "../mod.ts";
+
+Deno.test("fetchS", async function () {
+  assertEquals(
+    await fetchS("https://github.com") instanceof Uint8Array,
+    new Uint8Array() instanceof Uint8Array,
+  );
+});
 
 Deno.test("fetchSPolyfill", async function () {
   assertEquals(
