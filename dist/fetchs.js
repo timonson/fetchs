23028cb78c9a85c2c34a498b2f49c68dd0dec113
timonson <timo.rossa@tutanota.com>
2021-05-31 10:51:37 +0200

    Bundle to JavaScript

--- dist/fetchs.js
+++ dist/fetchs.js
@@ -1,42 +1,47 @@
 class FetchSError extends Error {
-    constructor(message, status, statusText){
-        super(message);
-        this.status = status;
-        this.statusText = statusText;
-        this.name = this.constructor.name;
-    }
+  status;
+  statusText;
+  constructor(message, status, statusText) {
+    super(message);
+    this.status = status;
+    this.statusText = statusText;
+    this.name = this.constructor.name;
+  }
 }
-const fetchS1 = async (url, init)=>{
-    const res = await fetch(url, init).catch((err)=>{
-        throw new FetchSError(err.message, 0, "Network Error");
-    });
-    if (!res.ok) {
-        throw new FetchSError(`Received status code ${res.status} instead of 200-299 range`, res.status, res.statusText);
+const fetchS1 = async (input, init) => {
+  const res = await fetch(input, init).catch((err) => {
+    throw new FetchSError(err.message, 0, "Network Error");
+  });
+  if (!res.ok) {
+    throw new FetchSError(
+      `Received status code ${res.status} instead of 200-299 range`,
+      res.status,
+      res.statusText,
+    );
+  }
+  try {
+    switch (init?.bodyMethod) {
+      case "arrayBuffer":
+        return await res.arrayBuffer();
+        break;
+      case "blob":
+        return await res.blob();
+        break;
+      case "formData":
+        return await res.formData();
+        break;
+      case "json":
+        return await res.json();
+        break;
+      case "text":
+        return await res.text();
+        break;
+      case "uint8Array":
+      default:
+        return new Uint8Array(await res.arrayBuffer());
     }
-    try {
-        switch(init?.bodyMethod){
-            case "arrayBuffer":
-                return await res.arrayBuffer();
-                break;
-            case "blob":
-                return await res.blob();
-                break;
-            case "formData":
-                return await res.formData();
-                break;
-            case "json":
-                return await res.json();
-                break;
-            case "text":
-                return await res.text();
-                break;
-            case "uint8Array":
-            default:
-                return new Uint8Array(await res.arrayBuffer());
-        }
-    } catch (err) {
-        throw new FetchSError(err.message, res.status, res.statusText);
-    }
+  } catch (err) {
+    throw new FetchSError(err.message, res.status, res.statusText);
+  }
 };
 export { fetchS1 as fetchS };
-
