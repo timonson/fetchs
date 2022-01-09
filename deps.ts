c8992a2b774833ec475142fe5ac62cf52628e110
timonson <timo.rossa@tutanota.com>
2022-01-09 20:17:37 +0100

    Bump versions

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { lookup } from "https://deno.land/x/media_types@v2.9.1/mod.ts";
-export { iter } from "https://deno.land/std@0.99.0/io/util.ts";
+export { lookup } from "https://deno.land/x/media_types@v2.11.1/mod.ts";
+export { iter } from "https://deno.land/std@0.120.0/io/util.ts";
