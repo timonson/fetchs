915814ddc8daae328f03208c33235e006e40c946
timonson <timo.rossa@tutanota.com>
2022-01-09 20:29:19 +0100

    Update bundle

--- dist/fetchs.js
+++ dist/fetchs.js
@@ -1,71 +1,44 @@
-var __awaiter = (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P ? value : new P(function (resolve) {
-        resolve(value);
-      });
-    }
-    return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-  };
 class FetchSError extends Error {
-  constructor(message, status, statusText) {
-    super(message);
-    this.status = status;
-    this.statusText = statusText;
-    this.name = this.constructor.name;
-  }
+    status;
+    statusText;
+    constructor(message, status, statusText){
+        super(message);
+        this.status = status;
+        this.statusText = statusText;
+        this.name = this.constructor.name;
+    }
 }
-export const fetchS = (input, init) =>
-  __awaiter(void 0, void 0, void 0, function* () {
-    const res = yield fetch(input, init).catch((err) => {
-      throw new FetchSError(err.message, 0, "Network Error");
+const fetchS = async (input, init)=>{
+    const res = await fetch(input, init).catch((err)=>{
+        throw new FetchSError(err.message, 0, "Network Error");
     });
+    return await processResponse(res, init);
+};
+async function processResponse(res, init) {
     if (!res.ok) {
-      throw new FetchSError(
-        `Received status code ${res.status} instead of 200-299 range`,
-        res.status,
-        res.statusText,
-      );
+        throw new FetchSError(`Received status code ${res.status} instead of 200-299 range`, res.status, res.statusText);
     }
     try {
-      switch (init === null || init === void 0 ? void 0 : init.bodyMethod) {
-        case "arrayBuffer":
-          return yield res.arrayBuffer();
-        case "blob":
-          return yield res.blob();
-        case "formData":
-          return yield res.formData();
-        case "json":
-          return yield res.json();
-        case "text":
-          return yield res.text();
-        case "uint8Array":
-          return new Uint8Array(yield res.arrayBuffer());
-        default:
-          return res.body;
-      }
+        switch(init?.bodyMethod){
+            case "arrayBuffer":
+                return await res.arrayBuffer();
+            case "blob":
+                return await res.blob();
+            case "formData":
+                return await res.formData();
+            case "json":
+                return await res.json();
+            case "text":
+                return await res.text();
+            case "uint8Array":
+                return new Uint8Array(await res.arrayBuffer());
+            default:
+                return res.body;
+        }
     } catch (err) {
-      throw new FetchSError(err.message, res.status, res.statusText);
+        throw new FetchSError(err.message, res.status, res.statusText);
     }
-  });
+}
+export { fetchS as fetchS };
+export { processResponse as processResponse };
+
