c52d2656c8d16381c9eaee4969ecb8df5f848596
timonson <timo.rossa@tutanota.com>
2022-01-09 20:30:10 +0100

    Fmt

--- dist/fetchs.js
+++ dist/fetchs.js
@@ -1,44 +1,47 @@
 class FetchSError extends Error {
-    status;
-    statusText;
-    constructor(message, status, statusText){
-        super(message);
-        this.status = status;
-        this.statusText = statusText;
-        this.name = this.constructor.name;
-    }
+  status;
+  statusText;
+  constructor(message, status, statusText) {
+    super(message);
+    this.status = status;
+    this.statusText = statusText;
+    this.name = this.constructor.name;
+  }
 }
-const fetchS = async (input, init)=>{
-    const res = await fetch(input, init).catch((err)=>{
-        throw new FetchSError(err.message, 0, "Network Error");
-    });
-    return await processResponse(res, init);
+const fetchS = async (input, init) => {
+  const res = await fetch(input, init).catch((err) => {
+    throw new FetchSError(err.message, 0, "Network Error");
+  });
+  return await processResponse(res, init);
 };
 async function processResponse(res, init) {
-    if (!res.ok) {
-        throw new FetchSError(`Received status code ${res.status} instead of 200-299 range`, res.status, res.statusText);
+  if (!res.ok) {
+    throw new FetchSError(
+      `Received status code ${res.status} instead of 200-299 range`,
+      res.status,
+      res.statusText,
+    );
+  }
+  try {
+    switch (init?.bodyMethod) {
+      case "arrayBuffer":
+        return await res.arrayBuffer();
+      case "blob":
+        return await res.blob();
+      case "formData":
+        return await res.formData();
+      case "json":
+        return await res.json();
+      case "text":
+        return await res.text();
+      case "uint8Array":
+        return new Uint8Array(await res.arrayBuffer());
+      default:
+        return res.body;
     }
-    try {
-        switch(init?.bodyMethod){
-            case "arrayBuffer":
-                return await res.arrayBuffer();
-            case "blob":
-                return await res.blob();
-            case "formData":
-                return await res.formData();
-            case "json":
-                return await res.json();
-            case "text":
-                return await res.text();
-            case "uint8Array":
-                return new Uint8Array(await res.arrayBuffer());
-            default:
-                return res.body;
-        }
-    } catch (err) {
-        throw new FetchSError(err.message, res.status, res.statusText);
-    }
+  } catch (err) {
+    throw new FetchSError(err.message, res.status, res.statusText);
+  }
 }
 export { fetchS as fetchS };
 export { processResponse as processResponse };
-
